9c64c87ca0ea387d7a8257d7e406b9ab19d1ce2e
葉奕緯 <aaa24295234@gmail.com>
2024-02-17 21:27:42 +0800

    index

---
+++ _nuxt/static/1708176319/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fcontact","\u002Fproduct-all","\u002Fproduct-muscle","\u002Farticle\u002F20220124","\u002Fproduct-detail","\u002Fnew-hale","\u002Farticles"]})
---
+++ _nuxt/static/1708176319/new-hale/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/new-hale", {data:[{}],fetch:{},mutations:void 0});
