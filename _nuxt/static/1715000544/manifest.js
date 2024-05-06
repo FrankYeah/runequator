17f7a875bc3722ae9b56f44a11972c42465ef558
葉奕緯 <aaa24295234@gmail.com>
2024-05-06 21:10:40 +0800

    add councel

--- _nuxt/static/1715000544/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fdealer","\u002Fcontact","\u002Farticle\u002F20220124","\u002Farticles","\u002Fproduct-all","\u002Fnew-hale","\u002Fproduct-detail","\u002Fproduct-muscle"]})
---
+++ _nuxt/static/1715000984/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdealer","\u002Fcontact","\u002Fabout","\u002Fproduct-detail","\u002Fproduct-all","\u002Fnew-hale","\u002Farticles","\u002Fproduct-muscle","\u002Farticle\u002F20220124"]})
