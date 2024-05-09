03243ce6c1bbe7cf63909c3d69f3727bb972871c
葉奕緯 <aaa24295234@gmail.com>
2024-05-09 09:31:55 +0800

    add dealer

--- _nuxt/static/1715000984/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdealer","\u002Fcontact","\u002Fabout","\u002Fproduct-detail","\u002Fproduct-all","\u002Fnew-hale","\u002Farticles","\u002Fproduct-muscle","\u002Farticle\u002F20220124"]})
